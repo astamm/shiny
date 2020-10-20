Corrected bug in diameter upper bound computation in gudhi-viz.

ShinyApps/gudhi-viz/gudhi-viz.py
@@ -129,9 +129,6 @@ def sync_figures(complexes, alpha, fps = 0, file = "temp-plot.html"):
         vertical_spacing = 0.05, 
         horizontal_spacing = 0.05
     )
-    max_rips  = np.max(np.abs(complexes["rips"]["points"]))
-    max_alpha = np.max(np.abs(complexes["alpha"]["points"]))
-    fig_max = max([max_rips, max_alpha])
     fig1 = compute_figure(
         points = complexes["rips"]["points"],
         triangles = get_triangles(
@@ -157,7 +154,7 @@ def sync_figures(complexes, alpha, fps = 0, file = "temp-plot.html"):
         showbackground = False, 
         showticklabels = False, 
         title_text = "", 
-        range = [-fig_max, fig_max]
+        range = [-1, 1]
     )
     scene_layout = dict(
         xaxis = axis_layout,